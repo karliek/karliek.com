5fc61c9afe4a50977ebf25728001214411fd9dec
karliek <karlie.kirkaldie@gmail.com>
2013-09-08 15:03:52 -0700

    karliek.com reset
    functional header/nav/hello sections

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+// clear typekit badge
+window.setTimeout(function() {
+	$('.typekit-badge').css('display', 'none');
+}, 0);
 
+var beResponsive = function() {
+	if ($(window).width() < 840) {
+		$('#skills li').addClass('mobile');
+	} else {
+		$('#skills li').removeClass('mobile');
+	}
+};
+
+// set the responsive handler for resizing
+$(window).on('resize', beResponsive);
+
+$(function() {
+
+	// run it once, in case the original load is narrow
+	beResponsive();
+
+});
